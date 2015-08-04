8f965ec760c977bc0e55439abc34b4f86df62cab
Kevin Overall <kmoverall@gmail.com>
2015-08-03 22:52:37 -0400

    More controls tweaking and level building, added an instant death zone

---
+++ Assets/DeathZone.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent (typeof(Collider2D))]
+public class DeathZone : MonoBehaviour {
+	void OnCollisionEnter2D (Collision2D coll) {
+		//Quick and simple: Restart the game on death
+		Application.LoadLevel(0);
+	}
+}
